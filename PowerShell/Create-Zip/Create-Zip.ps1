f8920968dda328aed9d57d6e62f489b1cd8d1f7e
Pierre JACQUOT <pierre.jacquot@gmx.com>
2017-06-27 14:17:32 +0200

    Update Create-Zip.ps1

--- PowerShell/Create-Zip/Create-Zip.ps1
+++ PowerShell/Create-Zip/Create-Zip.ps1
@@ -1,15 +1,15 @@
 <#
 .SYNOPSIS
-	Zip files creation
+    Zip files creation
 .DESCRIPTION
-	Create multiple zip files on a shared folder
+    Create multiple zip files on a shared folder
 .NOTES
-	File name : Create-Zip.ps1
-	Author : Pierre JACQUOT
-	Date : 08/06/2017
-	Version : 1.0
-.LINKS
-	Website : http://pierro.jacquot.free.fr
+    File name : Create-Zip.ps1
+    Author : Pierre JACQUOT
+    Date : 08/06/2017
+    Version : 1.0
+.LINK
+    Website : http://pierro.jacquot.free.fr
     Reference : http://pierro.jacquot.free.fr/index.php/scripts/34-script-create-zip
 #>
 
