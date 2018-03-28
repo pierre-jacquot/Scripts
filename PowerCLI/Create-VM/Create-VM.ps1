f08bb9d28296c780152bebadd8b2b8e3206e7288
Pierre JACQUOT <pierre.jacquot@gmx.com>
2018-03-28 22:34:18 +0200

    Update Create-VM.ps1

--- PowerCLI/Create-VM/Create-VM.ps1
+++ PowerCLI/Create-VM/Create-VM.ps1
@@ -8,6 +8,9 @@
     Author : Pierre JACQUOT
     Date : 20/06/2017
     Version : 1.0
+.LINK
+    Website : http://pierro.jacquot.free.fr
+    Reference : http://pierro.jacquot.free.fr/index.php/scripts/42-script-create-vm
 #>
 
 Clear-Host
