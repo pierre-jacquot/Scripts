5d76d6b3b04325ab37472b3229f11c40e4eef77a
Pierre JACQUOT <pierre.jacquot@gmx.com>
2018-12-02 19:17:42 +0100

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -9,8 +9,8 @@
     Date : 28/10/2018
     Version : 1.0
 .LINK
-    Website : http://pierro.jacquot.free.fr
-    Reference : http://pierro.jacquot.free.fr/index.php/scripts/44-configurer-son-profile-sous-windows-powershell-ise
+    Website : https://www.pierrejacquot.yo.fr
+    Reference : https://www.pierrejacquot.yo.fr/index.php/scripts/44-configurer-son-profile-sous-windows-powershell-ise
 #>
 
 Set-Location -Path "D:\"
