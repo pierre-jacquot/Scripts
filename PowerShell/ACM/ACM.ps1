b322a2e8044187b79be74b93020a3491c4fd901f
Pierre JACQUOT <pierre.jacquot@gmx.com>
2018-12-02 19:18:35 +0100

    Update ACM.ps1

--- PowerShell/ACM/ACM.ps1
+++ PowerShell/ACM/ACM.ps1
@@ -9,8 +9,8 @@
     Date : 13/10/2015
     Version : 1.0
 .LINK
-    Website : http://pierro.jacquot.free.fr
-    Reference : http://pierro.jacquot.free.fr/index.php/scripts/23-script-account-manager-acm
+    Website : https://www.pierrejacquot.yo.fr
+    Reference : https://www.pierrejacquot.yo.fr/index.php/scripts/23-script-account-manager-acm
 #>
 
 Import-Module ActiveDirectory
