6565f167553b89ed36cdd060749c1481c657cb02
Pierre JACQUOT <pierre.jacquot@gmx.com>
2018-12-02 19:24:37 +0100

    Update Purge-Folder.ps1

--- PowerShell/Purge-Folder/Purge-Folder.ps1
+++ PowerShell/Purge-Folder/Purge-Folder.ps1
@@ -9,8 +9,8 @@
     Date : 07/02/2018
     Version : 1.0
 .LINK
-    Website : http://pierro.jacquot.free.fr
-    Reference : http://pierro.jacquot.free.fr/index.php/scripts/43-script-purge-folder
+    Website : https://www.pierrejacquot.yo.fr
+    Reference : https://www.pierrejacquot.yo.fr/index.php/scripts/43-script-purge-folder
 #>
 
 Clear-Host
