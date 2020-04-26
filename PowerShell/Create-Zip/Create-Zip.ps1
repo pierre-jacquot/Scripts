1fbfa1731468d47094a571606937d6e22e07114e
Pierre JACQUOT <pierre.jacquot@gmx.com>
2020-04-26 17:51:56 +0200

    Update Create-Zip.ps1

--- PowerShell/Create-Zip/Create-Zip.ps1
+++ PowerShell/Create-Zip/Create-Zip.ps1
@@ -1,13 +1,13 @@
 <#
 .SYNOPSIS
-    Zip files creation.
+	Zip files creation
 .DESCRIPTION
-    Create multiple zip files on a shared folder.
+	Create multiple zip files on a shared folder
 .NOTES
-    File name : Create-Zip.ps1
-    Author : Pierre JACQUOT
-    Date : 08/06/2017
-    Version : 1.0
+	File name : Create-Zip.ps1
+	Author : Pierre JACQUOT
+	Date : 08/06/2017
+	Version : 1.0
 .LINK
     Website : https://www.pierrejacquot.yo.fr
     Reference : https://www.pierrejacquot.yo.fr/index.php/scripts/34-script-create-zip
@@ -20,23 +20,30 @@ Function Write-Log([string]$Output, [string]$Message) {
     ((Get-Date -UFormat "[%d-%m-%Y %H:%M:%S] ") + $Message) | Out-File -FilePath $Output -Append -Force
 }
 
-$StartTime = Get-Date
-$Hostname = [Environment]::MachineName
-$Login = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+[datetime]$StartTime = Get-Date
+[string]$Hostname = [Environment]::MachineName
+[string]$Login = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $Workfolder = Split-Path $script:MyInvocation.MyCommand.Path
 $Date = Get-Date -UFormat "%Y-%m-%d"
 $LogFile = $Workfolder + "\$Date-Create-Zip.log"
-$Records = Import-Csv -Path ".\Zip-Records.csv"
-$LineNumbers = $Records.Count
+$Records = @(Import-Csv -Path ".\Zip-Records.csv" -Delimiter "," -Encoding UTF8)
+[int]$LineNumbers = $Records.Count
+[string]$Activity = "Trying tp launch the creation of [$LineNumbers] zip file(s)"
+[int]$Step = 1
 
 Write-Host "Create-Zip :" -ForegroundColor Black -BackgroundColor Yellow
 Write-Host "Launching the creation of [$LineNumbers] zip file(s)." -ForegroundColor Cyan
 Write-Host "`r"
 
 ForEach ($Record in $Records) {
-    $User = $Record.User
-    $SourceFolder = $Record.SourceFolder+"$User"
-    $DestinationZip = $Record.DestinationZip+"$Date-Archive-$User.zip"
+    [string]$User = $Record.User
+    [string]$SourceFolder = $Record.SourceFolder+"$User\"
+    [string]$DestinationZip = $Record.DestinationZip+"$Date-Archive-$User.zip"
+    [string]$Status = "Processing [$Step] of [$LineNumbers] - $(([math]::Round((($Step)/$LineNumbers*100),0)))% completed"
+    [string]$CurrentOperation = "Creating zip file : $DestinationZip"
+    Write-Progress -Activity $Activity -Status $Status -CurrentOperation $CurrentOperation -PercentComplete ($Step/$LineNumbers*100)
+    $Step++
+    Start-Sleep -Seconds 1
     Try {
         Add-Type -Assembly "System.IO.Compression.FileSystem"
         [IO.Compression.ZipFile]::CreateFromDirectory($SourceFolder, $DestinationZip)
@@ -44,14 +51,14 @@ ForEach ($Record in $Records) {
         Write-Log -Output "$LogFile" -Message "The file $DestinationZip has been created"
     }
     Catch {
-        $ErrorMessage = $_.Exception.Message
+        [string]$ErrorMessage = $_.Exception.Message
         Write-Host "$ErrorMessage" -ForegroundColor Red
         Write-Log -Output "$LogFile" -Message "$ErrorMessage"
     }
 }
 
-$EndTime = Get-Date
-$Duration = [math]::Round((New-TimeSpan -Start $StartTime -End $EndTime).TotalMinutes,2)
+[datetime]$EndTime = Get-Date
+[decimal]$Duration = [math]::Round((New-TimeSpan -Start $StartTime -End $EndTime).TotalSeconds,2)
 
 Write-Host "`r"
 Write-Host "Script launched from : " -NoNewline; Write-Host $Hostname -ForegroundColor Red
@@ -59,5 +66,5 @@ Write-Host "By : " -NoNewline; Write-Host $Login -ForegroundColor Red
 Write-Host "Path : " -NoNewline; Write-Host $Workfolder -ForegroundColor Red
 Write-Host "Start time : " -NoNewline; Write-Host $StartTime -ForegroundColor Red
 Write-Host "End time : " -NoNewline; Write-Host $EndTime -ForegroundColor Red
-Write-Host "Duration : " -NoNewline; Write-Host $Duration -ForegroundColor Red -nonewline; Write-Host " minutes"
+Write-Host "Duration : " -NoNewline; Write-Host $Duration -ForegroundColor Red -nonewline; Write-Host " seconds"
 Write-Host "`r"
