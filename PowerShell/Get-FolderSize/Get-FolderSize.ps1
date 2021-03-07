cdffa9dad3c159c1f142e2919ed62b3c7406ebc7
Pierre JACQUOT <pierre.jacquot@gmx.com>
2021-03-07 14:03:37 +0100

    Update Get-FolderSize.ps1

--- PowerShell/Get-FolderSize/Get-FolderSize.ps1
+++ PowerShell/Get-FolderSize/Get-FolderSize.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
     Displays folders list with their size
 .DESCRIPTION
-    Displays and exports folders list with their size
+    Displays and export folders list with their size
 .NOTES
     File name : Get-FolderSize.ps1
     Author : Pierre JACQUOT
@@ -21,7 +21,7 @@ $StartTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 [string]$Workfolder = Split-Path $MyInvocation.MyCommand.Path
 [string]$Date = Get-Date -UFormat "%Y-%m-%d"
 [string]$BasePath = "D:\Scripts"
-[string]$CSVFile = $Workfolder + "\$Date-Folders.csv"
+[string]$CSVFile = $Workfolder + "\$Date-Folders-Export.csv"
 [string]$ReportFile = $Workfolder + "\$Date-Folders-Report.html"
 [array]$AllFolders = Get-Childitem -Path $BasePath -Directory -Force
 [int]$FolderNumbers = $AllFolders.Count
@@ -33,8 +33,6 @@ $StartTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 Write-Host "Get-FolderSize :" -ForegroundColor Black -BackgroundColor Yellow
 Write-Host "Launching the export of [$FolderNumbers] folder(s)." -ForegroundColor Cyan
 Write-Host "`r"
-
-[System.Collections.ArrayList]$FolderList = @()
 
 If ($FolderNumbers -eq 0) {
     Write-Warning "There is no folder in $BasePath"
@@ -72,24 +70,35 @@ Else {
         }
         $FolderList.Add($FolderObject) | Out-Null
     }
-    [array]$FoldersList = $FolderList | Sort-Object "Size (Bytes)" -Descending | Select-Object FolderName, "Size (Bytes)", "Size (MB)", "Size (GB)", CreationTime, LastWriteTime
-    $FoldersList | Format-Table -AutoSize
-    $FoldersList | Export-Csv -Path $CSVFile -NoTypeInformation -Delimiter ";" -Encoding UTF8
-
-    [string]$TotalSizeInBytes = "{0:N3} Bytes" -f ((Get-ChildItem -Path $BasePath -Recurse -Force | Measure-Object -Property Length -Sum).Sum)
-    [string]$TotalSizeInMB = "{0:N3} MB" -f ((Get-ChildItem -Path $BasePath -Recurse -Force | Measure-Object -Property Length -Sum).Sum / 1MB)
-    [string]$TotalSizeInGB = "{0:N3} GB" -f ((Get-ChildItem -Path $BasePath -Recurse -Force | Measure-Object -Property Length -Sum).Sum / 1GB)
-
-    $EndTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
-    [decimal]$Duration = [math]::Round((New-TimeSpan -Start $StartTime -End $EndTime).TotalSeconds,2)
-
-    [string]$PreContent = "<h1>$Title</h1><h2>Number of folder(s) : <span class='PostContentBlue'>$FolderNumbers</span></h2>"
-    [string]$PostContent = "<p>Total size : <span class='PostContentBlue'><strong>$TotalSizeInBytes</strong></span> - <span class='PostContentBlue'><strong>$TotalSizeInMB</strong></span> - <span class='PostContentBlue'><strong>$TotalSizeInGB</strong></span> on <span class='PostContentBlue'><strong>$BasePath</strong></span></p><p id='PostContent'>Script launched from : <span class='PostContentBlue'>$Hostname</span><br/>By : <span class='PostContentBlue'>$Login</span><br/>Path : <span class='PostContentBlue'>$Workfolder</span><br/>CSV file : <span class='PostContentBlue'>$(Split-Path $CSVFile -Leaf)</span><br/>Report file : <span class='PostContentBlue'>$(Split-Path $ReportFile -Leaf)</span><br/>Start time : <span class='PostContentBlue'>$StartTime</span><br/>End time : <span class='PostContentBlue'>$EndTime</span><br/>Duration : <span class='PostContentBlue'>$Duration</span> seconds</p>"
-    [string]$Report = $FoldersList | ConvertTo-Html -As Table -CssUri ".\Style.css" -Title $Title -PreContent $PreContent -PostContent $PostContent
-    $Report | Out-File -FilePath $ReportFile -Encoding utf8
-
-    Write-Host "Total size : $TotalSizeInBytes - $TotalSizeInMB - $TotalSizeInGB on [$BasePath]" -ForegroundColor Cyan
 }
+
+[array]$FoldersList = $FolderList | Sort-Object "Size (Bytes)" -Descending
+$FoldersList | Format-Table -AutoSize
+$FoldersList | Export-Csv -Path $CSVFile -NoTypeInformation -Delimiter ";" -Encoding UTF8
+
+[string]$TotalSizeInBytes = "{0:N3} Bytes" -f ((Get-ChildItem -Path $BasePath -Recurse -Force | Measure-Object -Property Length -Sum).Sum)
+[string]$TotalSizeInMB = "{0:N3} MB" -f ((Get-ChildItem -Path $BasePath -Recurse -Force | Measure-Object -Property Length -Sum).Sum / 1MB)
+[string]$TotalSizeInGB = "{0:N3} GB" -f ((Get-ChildItem -Path $BasePath -Recurse -Force | Measure-Object -Property Length -Sum).Sum / 1GB)
+
+$EndTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+[decimal]$Duration = [math]::Round((New-TimeSpan -Start $StartTime -End $EndTime).TotalSeconds,2)
+
+[string]$PreContent = "<h1>$Title</h1>
+<h2>Number of folder(s) : <span class='PostContentBlue'>$FolderNumbers</span></h2>"
+[string]$PostContent = "<p>Total size : <span class='PostContentBlue'><strong>$TotalSizeInBytes</strong></span> - <span class='PostContentBlue'><strong>$TotalSizeInMB</strong></span> - <span class='PostContentBlue'><strong>$TotalSizeInGB</strong></span> on <span class='PostContentBlue'><strong>$BasePath</strong></span></p>
+<p id='PostContent'>Script launched from : <span class='PostContentBlue'>$Hostname</span><br/>
+By : <span class='PostContentBlue'>$Login</span><br/>
+Path : <span class='PostContentBlue'>$Workfolder</span><br/>
+CSV file : <span class='PostContentBlue'>$(Split-Path $CSVFile -Leaf)</span><br/>
+Report file : <span class='PostContentBlue'>$(Split-Path $ReportFile -Leaf)</span><br/>
+Start time : <span class='PostContentBlue'>$StartTime</span><br/>
+End time : <span class='PostContentBlue'>$EndTime</span><br/>
+Duration : <span class='PostContentBlue'>$Duration</span> second(s)</p>"
+[string]$Report = $FoldersList | ConvertTo-Html -As Table -CssUri ".\Style.css" -Title $Title -PreContent $PreContent -PostContent $PostContent
+$Report | Out-File -FilePath $ReportFile -Encoding utf8
+
+Write-Host "Total size : $TotalSizeInBytes - $TotalSizeInMB - $TotalSizeInGB on [$BasePath]" -ForegroundColor Cyan
+
 Write-Host "`r"
 Write-Host "Script launched from : " -NoNewline; Write-Host $Hostname -ForegroundColor Red
 Write-Host "By : " -NoNewline; Write-Host $Login -ForegroundColor Red
@@ -98,5 +107,5 @@ Write-Host "CSV file : " -NoNewline; Write-Host (Split-Path $CSVFile -Leaf) -For
 Write-Host "Report file : " -NoNewline; Write-Host (Split-Path $ReportFile -Leaf) -ForegroundColor Red
 Write-Host "Start time : " -NoNewline; Write-Host $StartTime -ForegroundColor Red
 Write-Host "End time : " -NoNewline; Write-Host $EndTime -ForegroundColor Red
-Write-Host "Duration : " -NoNewline; Write-Host $Duration -ForegroundColor Red -nonewline; Write-Host " seconds"
+Write-Host "Duration : " -NoNewline; Write-Host $Duration -ForegroundColor Red -nonewline; Write-Host " second(s)"
 Write-Host "`r"
