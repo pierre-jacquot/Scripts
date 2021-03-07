d82fd3bdb3bafc2a100f714dbcd9846d8dba34e8
Pierre JACQUOT <pierre.jacquot@gmx.com>
2021-03-07 13:58:48 +0100

    Update Add-User.ps1

--- PowerShell/Add-User/Add-User.ps1
+++ PowerShell/Add-User/Add-User.ps1
@@ -25,6 +25,8 @@ $StartTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 [string]$Login = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 [string]$Workfolder = Split-Path $MyInvocation.MyCommand.Path
 [string]$Date = Get-Date -UFormat "%Y-%m-%d"
+[string]$CSVFile = $Workfolder + "\Add-User.csv"
+[string]$ReportFile = $Workfolder + "\$Date-Users-Report.html"
 [string]$LogFile = $Workfolder + "\$Date-Add-User.log"
 
 Write-Host "Add-User :" -ForegroundColor Black -BackgroundColor Yellow
@@ -41,16 +43,18 @@ Catch {
     Exit
 }
 Try {
-    [array]$Records = Import-Csv -Path ".\Add-User.csv" -Delimiter "," -Encoding UTF8
+    [array]$Records = Import-Csv -Path ".\Add-User.csv" -Delimiter ";" -Encoding UTF8
 }
 Catch {
     [string]$ErrorMessage = $_.Exception.Message
     Write-Host $ErrorMessage -ForegroundColor Red
     Write-Log -Output $LogFile -Message $ErrorMessage
 }
+$Records | Add-Member -Type NoteProperty -Name "Status" -Value "N/A"
 [int]$LineNumbers = $Records.Count
 [string]$Activity = "Trying to launch the addition of [$LineNumbers] user(s) into AD group(s)"
 [int]$Step = 1
+[string]$Title = "[$Date] - AD user(s) addition report on : $Hostname"
 
 If ((Test-Path ".\Add-User.csv") -eq $True -and $LineNumbers -eq 0) {
     Write-Warning "CSV file [Add-User.csv] is empty."
@@ -68,11 +72,13 @@ ElseIf ($LineNumbers -ge 1) {
         $Step++
         Start-Sleep -Seconds 1
         Try {
+            $Record.Status = "OK"
             Add-ADGroupMember -Identity $GroupName -Members $LoginName
             Write-Host "$LoginName has been added into the group : $GroupName." -ForegroundColor Green
             Write-Log -Output $LogFile -Message "$LoginName has been added into the group : $GroupName."
         }
         Catch {
+            $Record.Status = "KO"
             [string]$ErrorMessage = $_.Exception.Message
             Write-Host $ErrorMessage -ForegroundColor Red
             Write-Log -Output $LogFile -Message $ErrorMessage
@@ -83,13 +89,30 @@ ElseIf ($LineNumbers -ge 1) {
 
 $EndTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 [decimal]$Duration = [math]::Round((New-TimeSpan -Start $StartTime -End $EndTime).TotalSeconds,2)
+[string]$PreContent = "<h1>$Title</h1>
+<h2>Number of AD user(s) : <span class='PostContentBlue'>$LineNumbers</span></h2>"
+[string]$PostContent = "<p id='PostContent'>Script launched from : <span class='PostContentBlue'>$Hostname</span><br/>
+By : <span class='PostContentBlue'>$Login</span><br/>
+Path : <span class='PostContentBlue'>$Workfolder</span><br/>
+CSV file : <span class='PostContentBlue'>$(Split-Path $CSVFile -Leaf)</span><br/>
+Report file : <span class='PostContentBlue'>$(Split-Path $ReportFile -Leaf)</span><br/>
+Log file : <span class='PostContentBlue'>$(Split-Path $LogFile -Leaf)</span><br/>
+Start time : <span class='PostContentBlue'>$StartTime</span><br/>
+End time : <span class='PostContentBlue'>$EndTime</span><br/>
+Duration : <span class='PostContentBlue'>$Duration</span> second(s)</p>"
+[string]$Report = $Records | ConvertTo-Html -As Table -CssUri ".\Style.css" -Title $Title -PreContent $PreContent -PostContent $PostContent
+$Report = $Report -replace '<td>OK</td>','<td class="SuccessStatus">OK</td>'
+$Report = $Report -replace '<td>KO</td>','<td class="CriticalStatus">KO</td>'
+$Report | Out-File -FilePath $ReportFile -Encoding utf8
 
 Write-Host "`r"
 Write-Host "Script launched from : " -NoNewline; Write-Host $Hostname -ForegroundColor Red
 Write-Host "By : " -NoNewline; Write-Host $Login -ForegroundColor Red
 Write-Host "Path : " -NoNewline; Write-Host $Workfolder -ForegroundColor Red
+Write-Host "CSV file : " -NoNewline; Write-Host (Split-Path $CSVFile -Leaf) -ForegroundColor Red
+Write-Host "Report file : " -NoNewline; Write-Host (Split-Path $ReportFile -Leaf) -ForegroundColor Red
 Write-Host "Log file : " -NoNewline; Write-Host (Split-Path $LogFile -Leaf) -ForegroundColor Red
 Write-Host "Start time : " -NoNewline; Write-Host $StartTime -ForegroundColor Red
 Write-Host "End time : " -NoNewline; Write-Host $EndTime -ForegroundColor Red
-Write-Host "Duration : " -NoNewline; Write-Host $Duration -ForegroundColor Red -nonewline; Write-Host " seconds"
+Write-Host "Duration : " -NoNewline; Write-Host $Duration -ForegroundColor Red -nonewline; Write-Host " second(s)"
 Write-Host "`r"
