5aa811194bdce818371fb4fda0a61f305262138f
Pierre JACQUOT <pierre.jacquot@gmx.com>
2021-03-07 14:02:41 +0100

    Update Find-User.ps1

--- PowerShell/Find-User/Find-User.ps1
+++ PowerShell/Find-User/Find-User.ps1
@@ -25,12 +25,16 @@ $StartTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 [string]$Login = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 [string]$Workfolder = Split-Path $MyInvocation.MyCommand.Path
 [string]$Date = Get-Date -UFormat "%Y-%m-%d"
+[string]$TXTFile = $Workfolder + "\Logins.txt"
+[string]$ReportFile = $Workfolder + "\$Date-Users-Report.html"
 [string]$LogFileOK = $Workfolder + "\$Date-Users_OK.log"
 [string]$LogFileKO = $Workfolder + "\$Date-Users_KO.log"
 [array]$Logins = Get-Content -Path ".\Logins.txt" -ErrorAction SilentlyContinue
 [int]$LineNumbers = $Logins.Count
+[System.Collections.ArrayList]$UserList = @()
 [string]$Activity = "Trying to launch the research of [$LineNumbers] user(s) in AD"
 [int]$Step = 1
+[string]$Title = "[$Date] - AD user(s) report on : $Hostname"
 
 Write-Host "Find-User :" -ForegroundColor Black -BackgroundColor Yellow
 Try {
@@ -73,23 +77,54 @@ Else {
             Write-Log -Output $LogFileKO -Message $ErrorMessage
         }
         If ($User -eq $Null) {
+            [string]$UserExist = "KO"
             Write-Host "$ADLogin - User does not exist in AD." -ForegroundColor Red
             Write-Log -Output $LogFileKO -Message "$ADLogin - User does not exist in AD."
         }
         Else {
+            $UserExist = "OK"
             Write-host "$ADLogin - User found in AD." -ForegroundColor Green
             Write-Log -Output $LogFileOK -Message "$ADLogin - User found in AD."
         }
+        $ServerObject = [PSCustomObject]@{
+            sAMAccountName = $ADLogin
+            Status = $UserExist
+        }
+        $UserList.Add($ServerObject) | Out-Null
     }
 }
 
 $EndTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 [decimal]$Duration = [math]::Round((New-TimeSpan -Start $StartTime -End $EndTime).TotalSeconds,2)
+[string]$PreContent = "<h1>$Title</h1>
+<h2>Number of AD user(s) : <span class='PostContentBlue'>$LineNumbers</span></h2>"
+[string]$SuccessLogFile = "Success log file : <span class='PostContentBlue'>$(Split-Path $LogFileOK -Leaf)</span><br/>"
+[string]$WarningLogFile = "Warning log file : <span class='PostContentBlue'>$(Split-Path $LogFileKO -Leaf)</span><br/>"
+[string]$PostContent = "<p id='PostContent'>Script launched from : <span class='PostContentBlue'>$Hostname</span><br/>
+By : <span class='PostContentBlue'>$Login</span><br/>
+Path : <span class='PostContentBlue'>$Workfolder</span><br/>
+TXT file : <span class='PostContentBlue'>$(Split-Path $TXTFile -Leaf)</span><br/>
+Report file : <span class='PostContentBlue'>$(Split-Path $ReportFile -Leaf)</span><br/>
+$(If ((Test-Path $LogFileOK) -eq $True) { 
+    $SuccessLogFile
+})
+$(If ((Test-Path $LogFileKO) -eq $True) { 
+    $WarningLogFile
+})
+Start time : <span class='PostContentBlue'>$StartTime</span><br/>
+End time : <span class='PostContentBlue'>$EndTime</span><br/>
+Duration : <span class='PostContentBlue'>$Duration</span> second(s)</p>"
+[string]$Report = $UserList | ConvertTo-Html -As Table -CssUri ".\Style.css" -Title $Title -PreContent $PreContent -PostContent $PostContent
+$Report = $Report -replace '<td>OK</td>','<td class="SuccessStatus">OK</td>'
+$Report = $Report -replace '<td>KO</td>','<td class="CriticalStatus">KO</td>'
+$Report | Out-File -FilePath $ReportFile -Encoding utf8
 
 Write-Host "`r"
 Write-Host "Script launched from : " -NoNewline; Write-Host $Hostname -ForegroundColor Red
 Write-Host "By : " -NoNewline; Write-Host $Login -ForegroundColor Red
 Write-Host "Path : " -NoNewline; Write-Host $Workfolder -ForegroundColor Red
+Write-Host "TXT file : " -NoNewline; Write-Host (Split-Path $TXTFile -Leaf) -ForegroundColor Red
+Write-Host "Report file : " -NoNewline; Write-Host (Split-Path $ReportFile -Leaf) -ForegroundColor Red
 If ((Test-Path $LogFileOK) -eq $True) {
     Write-Host "Log file : " -NoNewline; Write-Host (Split-Path $LogFileOK -Leaf) -ForegroundColor Red
 }
@@ -98,5 +133,5 @@ If ((Test-Path $LogFileKO) -eq $True) {
 }
 Write-Host "Start time : " -NoNewline; Write-Host $StartTime -ForegroundColor Red
 Write-Host "End time : " -NoNewline; Write-Host $EndTime -ForegroundColor Red
-Write-Host "Duration : " -NoNewline; Write-Host $Duration -ForegroundColor Red -nonewline; Write-Host " seconds"
+Write-Host "Duration : " -NoNewline; Write-Host $Duration -ForegroundColor Red -nonewline; Write-Host " second(s)"
 Write-Host "`r"
