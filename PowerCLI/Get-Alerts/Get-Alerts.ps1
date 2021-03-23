82efcff8af1621d5f4ecb7dc8587d1b3c6922af8
Pierre JACQUOT <pierre.jacquot@gmx.com>
2021-03-23 22:00:40 +0100

    Update Get-Alerts.ps1

--- PowerCLI/Get-Alerts/Get-Alerts.ps1
+++ PowerCLI/Get-Alerts/Get-Alerts.ps1
@@ -24,24 +24,23 @@ $StartTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 [string]$CSVFile = $Workfolder + "\$Date-Alerts-Export.csv"
 [string]$ReportFile = $Workfolder + "\$Date-Alerts-Report.html"
 [string]$vCenter = "vCenterServerName"
-[string]$Table = $Null
 [System.Collections.ArrayList]$AlertList = @()
 
 Write-Host "Get-Alerts :" -ForegroundColor Black -BackgroundColor Yellow
 
 # Trying to connect to the VMware vCenter Server
 Try {
-    $Connexion = Connect-VIServer -Server $vCenter
+	$Connexion = Connect-VIServer -Server $vCenter
     Write-Host "Connected on the VMware vCenter Server : $Connexion" -ForegroundColor Green
 }
 Catch {
-    Write-Host "[ERROR] : Unable to connect on the VMware vCenter Server" -ForegroundColor Red
-    Exit
+	Write-Host "[ERROR] : Unable to connect on the VMware vCenter Server" -ForegroundColor Red
+	Exit
 }
 
 # Retrieves and count alert(s) on the VMware vCenter Server
 $rootFolder = Get-Folder -NoRecursion
-$Alerts = ($rootFolder.ExtensionData.TriggeredAlarmState) | Sort-Object Time -Descending | Select-Object *
+$Alerts = ($rootFolder.ExtensionData.TriggeredAlarmState) | Sort-Object Time -Descending
 [int]$AlertsNumber = $Alerts.Count
 
 If ($AlertsNumber -ge 1) {
@@ -52,32 +51,21 @@ If ($AlertsNumber -ge 1) {
     [string]$MailSubject = "[$Date] - [VMware] - Alert(s) daily report on : $vCenter"
     [string]$MailSMTPServer = "SMTPServerName"
 
-    [string]$MailBody = '<html>'
-    $MailBody += '<head>'
-    $MailBody += "<title>$MailSubject</title>"
-    $MailBody += '<style type="text/css">'
-    $MailBody += 'h1 { font-family: Arial; color: #e68a00; font-size: 28px; }'
-    $MailBody += 'h2 { font-family: Arial; color: #000000; font-size: 16px; }'
-    $MailBody += '.customTable { border: 1px solid #000000; }'
-    $MailBody += '.customTable table { border-collapse: collapse; border-spacing: 0; margin: 0px; padding: 0px; }'
-    $MailBody += '.customTable th { background-color: #d3e5d4; font-size: 14px; font-family: Arial; font-weight: bold; }'
-    $MailBody += '.customTable td { font-size: 13px; font-family: Arial; }'
-    $MailBody += '.customTable tr:nth-child(even) { background-color: #f0f0f2; }'
-    $MailBody += '.customTable tr:hover { background-color: #ddd; }'
-    $MailBody += '.CriticalStatus { background-color: #ff0000; font-weight: bold; color: #ffffff; }'
-    $MailBody += '.WarningStatus { background-color: #ffa500; font-weight: bold; color: #ffffff; }'
-    $MailBody += '#PostInfo { font-family: Arial; font-size: 11px; font-style: italic; }'
-    $MailBody += 'span.Info { color: #000099; }'
-    $MailBody += '</style>'
-    $MailBody += '</head>'
-
-    $MailBody += '<body>'
-    $MailBody += "<h1>$MailSubject</h1>"
-    $MailBody += "<h2>Number of alert(s) : <span class='Info'>$AlertsNumber</span></h2>"
-
-    # Prepare Table
-    $Table += '<table class="customTable">'
-    $Table += "<tr><th>Time</th><th>EntityType</th><th>Entity</th><th>Alarm</th><th>Acknowledged</th><th>AckBy</th><th>AckTime</th><th>Status</th></tr>"
+    [string]$Style="<title>$MailSubject</title>
+    <style>
+        h1 { font-family: Arial; color: #e68a00; font-size: 28px; }
+        h2 { font-family: Arial; color: #000000; font-size: 16px; }
+        table { border-collapse: collapse; }
+        td, th { border: 1px solid #000000; }
+        td { font-size: 13px; font-family: Arial; }
+        th { background-color: #d3e5d4; font-size: 14px; font-family: Arial; font-weight: bold; text-align: left; }
+        tr:nth-child(even) { background-color: #f0f0f2; }
+        tr:hover { background-color: #ddd; }
+        .CriticalStatus { background-color: #ff0000; font-weight: bold; color: #ffffff; }
+        .WarningStatus { background-color: #ffa500; font-weight: bold; color: #ffffff; }
+        #PostContent { font-family: Arial; font-size: 11px; font-style: italic; }
+        span.PostContentBlue { color: #000099; }
+    </style>"
 
     Foreach ($Alert in $Alerts) {
         [string]$AlarmAlarm = (Get-View $Alert.Alarm).Info.Name
@@ -101,9 +89,7 @@ If ($AlertsNumber -ge 1) {
             $AlarmStatus = "Warning"
         }
 
-        $Table += "<tr><td>$AlarmTime</td><td>$AlarmEntityType</td><td>$AlarmEntity</td><td>$AlarmAlarm</td><td>$AlarmAcknowledged</td><td>$AlarmAckBy</td><td>$AlarmAckTime</td><td>$AlarmStatus</td></tr>"
-
-        $ServerObject = [PSCustomObject]@{
+        $ParamList = [PSCustomObject]@{
             Time = $AlarmTime
             EntityTime = $AlarmEntityType
             Entity = $AlarmEntity
@@ -113,37 +99,35 @@ If ($AlertsNumber -ge 1) {
             AckTime = $AlarmAckTime
             Status = $AlarmStatus
         }
-        $AlertList.Add($ServerObject) | Out-Null
+        $AlertList.Add($ParamList) | Out-Null
     }
 
-    $Table += "</table>"
-
+    $AlertList = $AlertList | Sort-Object Time -Descending
+    $AlertList | Format-Table -AutoSize
     $AlertList | Export-Csv -Path $CSVFile -NoTypeInformation -Delimiter ";" -Encoding UTF8
+    
     $EndTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
     [decimal]$Duration = [math]::Round((New-TimeSpan -Start $StartTime -End $EndTime).TotalSeconds,2)
 
-    [string]$Info = "<p id='PostInfo'>Script launched from : <span class='Info'>$Hostname</span><br/>
-    By : <span class='Info'>$Login</span><br/>
-    Path : <span class='Info'>$Workfolder</span><br/>
-    CSV file : <span class='Info'>$(Split-Path $CSVFile -Leaf)</span><br/>
-    Report file : <span class='Info'>$(Split-Path $ReportFile -Leaf)</span><br/>
-    Start time : <span class='Info'>$StartTime</span><br/>
-    End time : <span class='Info'>$EndTime</span><br/>
-    Duration : <span class='Info'>$Duration</span> second(s)</p>"
+    [string]$PreContent = "<h1>$MailSubject</h1>
+    <h2>Number of alert(s) : <span class='PostContentBlue'>$AlertsNumber</span></h2>"
+    [string]$PostContent = "<p id='PostContent'>Script launched from : <span class='PostContentBlue'>$Hostname</span><br/>
+    By : <span class='PostContentBlue'>$Login</span><br/>
+    Path : <span class='PostContentBlue'>$Workfolder</span><br/>
+    CSV file : <span class='PostContentBlue'>$(Split-Path $CSVFile -Leaf)</span><br/>
+    Report file : <span class='PostContentBlue'>$(Split-Path $ReportFile -Leaf)</span><br/>
+    Start time : <span class='PostContentBlue'>$StartTime</span><br/>
+    End time : <span class='PostContentBlue'>$EndTime</span><br/>
+    Duration : <span class='PostContentBlue'>$Duration</span> second(s)</p>"
 
-    $MailBody += $Table
-    $MailBody += $Info
-    $MailBody += '</body>'
-    $MailBody += '</html>'
-
-    $MailBody = $MailBody -replace '<td>Warning</td>','<td class="WarningStatus">Warning</td>'
-    $MailBody = $MailBody -replace '<td>Alert</td>','<td class="CriticalStatus">Alert</td>'
-    $MailBody | Out-File -FilePath $ReportFile
-
-    $AlertList | Format-Table
+    [string]$Report = $AlertList | ConvertTo-Html -As Table -Head $Style -PreContent $PreContent -PostContent $PostContent
+    $Report = $Report -replace '<td>Warning</td>','<td class="WarningStatus">Warning</td>'
+    $Report = $Report -replace '<td>Alert</td>','<td class="CriticalStatus">Alert</td>'
+    $Report | Out-File -FilePath $ReportFile -Encoding utf8
+    Write-Host "[VMware] - Alert(s) daily report has been created : $ReportFile" -ForegroundColor Green
     
     Try {
-        Send-MailMessage -From $MailFrom -To $MailTo -Cc $MailCc -Subject $MailSubject -Body $MailBody -Priority High -Attachments $ReportFile -SmtpServer $MailSMTPServer -BodyAsHtml -Encoding UTF8
+        Send-MailMessage -From $MailFrom -To $MailTo -Cc $MailCc -Subject $MailSubject -Body $Report -Priority High -Attachments $ReportFile -SmtpServer $MailSMTPServer -BodyAsHtml -Encoding UTF8
         Write-Host "VMware alerts(s) report with attached file : $(Split-Path $ReportFile -Leaf) has been sent by e-mail" -ForegroundColor Green
     }
     Catch {
