67b1b3d16f6f4aa79f012613f89e85be3582a04d
Pierre JACQUOT <pierre.jacquot@gmx.com>
2021-03-28 15:26:35 +0200

    Update Ping-Server.ps1

--- PowerShell/Ping-Server/Ping-Server.ps1
+++ PowerShell/Ping-Server/Ping-Server.ps1
@@ -9,8 +9,8 @@
     Date : 27/10/2015
     Version : 1.0
 .LINK
-    Website : https://www.pierrejacquot.yo.fr
-    Reference : https://www.pierrejacquot.yo.fr/index.php/scripts/22-script-ping-server
+    Website : https://www.pierrejacquot.go.yo.fr
+    Reference : https://www.pierrejacquot.go.yo.fr/index.php/scripts/22-script-ping-server
 #>
 
 Clear-Host
