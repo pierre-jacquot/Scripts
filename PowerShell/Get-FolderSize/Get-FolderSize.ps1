50a8a3661ae5e2f28c7bb4ee62fe242b06fb58ee
Pierre JACQUOT <pierre.jacquot@gmx.com>
2021-03-28 15:24:41 +0200

    Update Get-FolderSize.ps1

--- PowerShell/Get-FolderSize/Get-FolderSize.ps1
+++ PowerShell/Get-FolderSize/Get-FolderSize.ps1
@@ -9,8 +9,8 @@
     Date : 19/07/2017
     Version : 1.0
 .LINK
-    Website : https://www.pierrejacquot.yo.fr
-    Reference : https://www.pierrejacquot.yo.fr/index.php/scripts/39-script-get-foldersize
+    Website : https://www.pierrejacquot.go.yo.fr
+    Reference : https://www.pierrejacquot.go.yo.fr/index.php/scripts/39-script-get-foldersize
 #>
 
 Clear-Host
