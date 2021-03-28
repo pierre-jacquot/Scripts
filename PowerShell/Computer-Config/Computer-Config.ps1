e884567f2920ed80bf42aa76c0aab03ab36f622f
Pierre JACQUOT <pierre.jacquot@gmx.com>
2021-03-28 15:22:08 +0200

    Update Computer-Config.ps1

--- PowerShell/Computer-Config/Computer-Config.ps1
+++ PowerShell/Computer-Config/Computer-Config.ps1
@@ -9,8 +9,8 @@
     Date : 10/11/2015
     Version : 1.0
 .LINK
-    Website : https://www.pierrejacquot.yo.fr
-    Reference : https://www.pierrejacquot.yo.fr/index.php/scripts/25-script-computer-config
+    Website : https://www.pierrejacquot.go.yo.fr
+    Reference : https://www.pierrejacquot.go.yo.fr/index.php/scripts/25-script-computer-config
 #>
 
 Clear-Host
